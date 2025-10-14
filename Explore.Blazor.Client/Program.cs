d68ae5f5583d6272ccdb5f74f36296d6840d6218
WassimYezeiph <wassim.yezeiph@student.odisee.be>
2025-10-14 09:09:10 +0200

    style(CreateEvent): fix page

--- Explore.Blazor.Client/Program.cs
+++ Explore.Blazor.Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped<IEventService, EventService>();
-builder.Services.AddScoped<HttpClient>();
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
