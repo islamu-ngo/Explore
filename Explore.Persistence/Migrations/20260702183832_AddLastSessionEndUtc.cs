cbcec7085e0df3779a9b535fc92c55afb7e2b854
Amir Akrari <amirakrari@gmail.com>
2026-07-02 20:46:27 +0200

    feat(persistence/events): generate migration for LastSessionEndUtc column

---
+++ Explore.Persistence/Migrations/20260702183832_AddLastSessionEndUtc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Explore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastSessionEndUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "last_session_end_utc",
+                table: "events",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_session_end_utc",
+                table: "events");
+        }
+    }
+}
