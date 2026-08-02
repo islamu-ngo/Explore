56c0fcfb2860ec54005955a01698bd44325b6b17
Amir Akrari <amirakrari@gmail.com>
2026-08-02 21:56:29 +0200

    chore(migrations/sqlserver): add initial stores

---
+++ src/Explore.Persistence.DataProtection.Migrations.SqlServer/Migrations/20260802191644_InitialSqlServerDataProtection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Explore.Persistence.DataProtection.Migrations.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlServerDataProtection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "islamu_event");
+
+            migrationBuilder.CreateTable(
+                name: "data_protection_keys",
+                schema: "islamu_event",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    friendly_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    xml = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_data_protection_keys", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "data_protection_keys",
+                schema: "islamu_event");
+        }
+    }
+}
