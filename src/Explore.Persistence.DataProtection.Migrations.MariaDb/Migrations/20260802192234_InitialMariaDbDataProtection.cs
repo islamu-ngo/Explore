2b3422e543cc3e571df7edf0a5846a23e8d4e536
Amir Akrari <amirakrari@gmail.com>
2026-08-02 21:56:33 +0200

    chore(migrations/mariadb): add initial stores

---
+++ src/Explore.Persistence.DataProtection.Migrations.MariaDb/Migrations/20260802192234_InitialMariaDbDataProtection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Explore.Persistence.DataProtection.Migrations.MariaDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMariaDbDataProtection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "islamu_event_data_protection_keys",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    friendly_name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    xml = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_islamu_event_data_protection_keys", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "islamu_event_data_protection_keys");
+        }
+    }
+}
