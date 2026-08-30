7ad222c285e5e0e8ff8e8c9f12bf7acb29fca6ba
Amir Akrari <amirakrari@gmail.com>
2026-08-30 13:58:28 +0200

    fix(database): complete MySQL portability migration
    Add the generated MySQL legal-document migration and align its model snapshot.
    
    Changelog: skip
    
    Changelog-Reason: internal generated migration completion

--- src/Explore.Persistence.Migrations.MySql/Migrations/20260830115812_AddPortableLegalDocuments.Designer.cs
+++ src/Explore.Persistence.Migrations.MySql/Migrations/20260830115812_AddPortableLegalDocuments.Designer.cs
@@ -4,6 +4,7 @@ using Explore.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Explore.Persistence.Migrations.MySql.Migrations
 {
     [DbContext(typeof(ExploreDbContext))]
-    partial class ExploreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260830115812_AddPortableLegalDocuments")]
+    partial class AddPortableLegalDocuments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
